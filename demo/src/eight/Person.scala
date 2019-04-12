58ec5b3058838cba6f1a872e33e439aa76d17cc8
kakayou <liuj.zzu@icloud.com>
2019-04-13 00:36:31 +0800

    add note

--- demo/src/eight/Person.scala
+++ demo/src/eight/Person.scala
@@ -1,12 +1,13 @@
 package eight
-
-case class Person {
+/**
+  * 主构造器与类定义交织在一起
+  *
+  * */
+class Person(val name:String) {
   private var privateAge = 0
   def age =privateAge
   def age_=(newValue: Int): Unit ={
     if (newValue > privateAge) privateAge=newValue
   }
-  def name =""
-
   override def toString: String = getClass.getName + "[name="+name+"]"
 }
